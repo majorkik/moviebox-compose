196e8170e19305feaa254147b539beb2aca51aad
Rodion Belovitskiy <belovickiy.rodion@gmail.com>
2022-04-19 23:29:20 +0300

    feat: add ktlint & spotless plugin

--- buildSrc/src/main/kotlin/linter/CodeQualityPlugin.kt
+++ buildSrc/src/main/kotlin/linter/CodeQualityPlugin.kt
@@ -7,5 +7,7 @@ import org.gradle.kotlin.dsl.apply
 class CodeQualityPlugin : Plugin<Project> {
     override fun apply(target: Project) = target.run {
         apply<AppDetektPlugin>()
+        apply<AppKtlintPlugin>()
+        apply<AppSpotlessPlugin>()
     }
 }
