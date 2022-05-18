95318ad19b3014484a2078d5eb36347e754fb3b1
Rodion Belovitskiy <belovickiy.rodion@gmail.com>
2022-05-18 23:55:53 +0300

    feat: update dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
 
     implementation("com.github.ben-manes:gradle-versions-plugin:0.42.0")
     implementation("org.jlleitschuh.gradle:ktlint-gradle:10.2.1")
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.4.2")
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.6.1")
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.20.0")
-    implementation("io.arrow-kt.analysis.kotlin:io.arrow-kt.analysis.kotlin.gradle.plugin:2.0.2-alpha.18")
+    implementation("io.arrow-kt.analysis.kotlin:io.arrow-kt.analysis.kotlin.gradle.plugin:2.0.2")
 }
